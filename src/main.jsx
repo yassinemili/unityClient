ae8b6101a3a5bfee7b1ecce4be2eba6491a1e75f
Yassine Mili <ya.mili@icloud.com>
2025-03-10 02:35:52 +0100

    adding the AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import AuthProvider from "./context/AuthProvider.jsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
