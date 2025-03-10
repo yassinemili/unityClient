820d47b21b2e6769ca518bdda1efbfafef7ec10a
Yassine Mili <ya.mili@icloud.com>
2025-03-11 00:47:38 +0100

    fixing routes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -213,7 +213,7 @@ export default function Header() {
               </div>
               <div className="py-6">
                 <Link
-                  to="/"
+                  to="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
