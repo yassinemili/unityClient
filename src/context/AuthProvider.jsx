7843600811cd129eacb9e469bd6e62a2f6e2d823
Yassine Mili <ya.mili@icloud.com>
2025-03-11 06:17:50 +0100

    fixing some bugs of token

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,21 +1,17 @@
-// src/context/AuthProvider.jsx
 import { useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import { AuthContext } from "../hooks/useAuth.jsx";
 
 const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
 
   const login = async (credentials) => {
-
     if (!credentials.username || !credentials.password) {
       throw new Error("Username and password are required");
     }
 
-
-    if (token || user) {
+    if (user) {
       throw new Error("User is already logged in");
     }
 
@@ -26,10 +22,8 @@ const AuthProvider = ({ children }) => {
         { withCredentials: true }
       );
 
-      const { accessToken, user } = response.data;
-      setToken(accessToken);
+      const { user } = response.data;
       setUser(user);
-      localStorage.setItem("user", JSON.stringify(user));
       return response.data;
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
@@ -43,13 +37,12 @@ const AuthProvider = ({ children }) => {
       {},
       { withCredentials: true }
     );
-    setToken(null);
     setUser(null);
     window.location.href = "/";
   };
 
   return (
-    <AuthContext.Provider value={{ token, user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
