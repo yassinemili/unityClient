a3b615013680338965fbb1ba84d2f2dfd2f2449c
Yassine Mili <ya.mili@icloud.com>
2025-03-15 03:50:28 +0100

    use the theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./context/AuthProvider.jsx";
+import { ThemeProvider } from "next-themes";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
