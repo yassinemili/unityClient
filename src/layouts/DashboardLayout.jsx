c0bcff4c37b48c9599b79aff640001f7f8c966ee
Yassine Mili <ya.mili@icloud.com>
2025-03-15 02:36:23 +0100

    adding the sidebar to the top of the dahsbord layout.

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="flex min-h-screen">
       {/*put the side bar here*/}
+      <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
       <div className="flex flex-col flex-1">
         {/*put the nav bar of dashboard here*/}
         <main className="p-6">
